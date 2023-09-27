48c20788d68d21dcefdfa335c8a417094beccb52
Alex <example@gmail.com>
2023-09-27 21:34:13 +0300

    exercises with for loop

--- for-exercises/exercise-figure.js
+++ for-exercises/exercise-figure.js
@@ -1,1 +1,14 @@
-console.log('rere')
+const lines = 5;
+let result = '';
+
+for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' ';
+    }
+
+    for (let j = 0; i < 2 * i + 1; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
