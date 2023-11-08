7e63d019a1e69c1a59f9c4b14e1bbf2b2317dec4
Alex <example@gmail.com>
2023-11-08 10:14:49 +0200

    basic animation with cube

--- 43/js/script.js
+++ 43/js/script.js
@@ -12,3 +12,29 @@ box.style.width = '500px';
 btns.style.width = '500px';
 
 btns[1].style.borderRadius = '100%';
+
+///
+/// work with setTimeout
+const btn = document.querySelector('.btn');
+let timeId,
+    i = 0;
+
+btn.addEventListener('click', () => {
+    // const timeId = setTimeout(logger, 2000);
+    timerId = setInterval(logger, 500);
+});
+
+clearInterval(timeId);
+
+function logger() {
+    if (i === 3) {
+        clearInterval(timeId);
+    }
+    console.log('text');
+    i++;
+}
+
+let id = setTimeout(function log() {
+    console.log('Hello');
+    id = setTimeout(log, 500);
+}, 500);
