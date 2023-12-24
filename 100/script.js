8608ddb89befd507241c0fa26f8f0ac752f54a76
Alex <example@gmail.com>
2023-12-24 13:31:49 +0200

    Food project - working with webpack

--- 100/script.js
+++ 100/script.js
@@ -25,6 +25,27 @@ class User {
     }
 }
 
+// function User1(name, age) {
+//     this.name = name;
+//     let userAge = age;
+
+//     say = () => {
+//         console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+//     }
+
+//     this.getAge = function () {
+//         return this._age;
+//     }
+
+//     this.setAge = function (age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             this._age = age;
+//         } else {
+//             console.log('Недопустимое значение');
+//         }
+//     }
+// }
+
 const ivan = new User('Ivan', 27);
 console.log(ivan.surname);
 
