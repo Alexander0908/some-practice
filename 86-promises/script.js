7cbed77987b220d190afa060c992f45f2e56393d
Alex <example@gmail.com>
2024-01-16 00:09:21 +0200

    practice with promises

--- 86-promises/script.js
+++ 86-promises/script.js
@@ -1,4 +1,4 @@
-// 'use script';
+'use script';
 
 // console.log('Запрос данных...');
 
@@ -48,4 +48,41 @@ const test = time => {
 
 Promise.race([test(1000), test(2000)]).then(() => {
     console.log('All');
+});
+
+
+
+////////// 14.01.24
+
+console.log('Запрос данных...');
+
+const req = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Подготовка данных...');
+
+        const product = {
+            name: 'TV',
+            price: 2000
+        }
+
+        resolve(product);
+    }, 2000);
+});
+
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then(data => {
+    console.log(data);
+}).catch(() => {
+    console.error('Произошла ошибка');
+}).finally(() => {
+    console.log('Finally');
 });
