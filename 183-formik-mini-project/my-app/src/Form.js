84fa643027aa9254ed859cc2c0f6f79522627b5e
Alexander0908 <sanya090895@gmail.com>
2024-07-24 21:06:35 +0300

    Updated language
    Updated language

--- 183-formik-mini-project/my-app/src/Form.js
+++ 183-formik-mini-project/my-app/src/Form.js
@@ -3,20 +3,20 @@
 const Form = () => {
     return (
         <form className="form">
-            <h2>Отправить пожертвование</h2>
-            <label htmlFor="name">Ваше имя</label>
+            <h2>Відправити пожертвування</h2>
+            <label htmlFor="name">Ваше ім'я</label>
             <input
                 id="name"
                 name="name"
                 type="text"
             />
-            <label htmlFor="email">Ваша почта</label>
+            <label htmlFor="email">Ваша пошта</label>
             <input
                 id="email"
                 name="email"
                 type="email"
             />
-            <label htmlFor="amount">Количество</label>
+            <label htmlFor="amount">Кількість</label>
             <input
                 id="amount"
                 name="amount"
@@ -26,21 +26,21 @@ const Form = () => {
             <select
                 id="currency"
                 name="currency">
-                    <option value="">Выберите валюту</option>
+                    <option value="">Оберіть валюту</option>
                     <option value="USD">USD</option>
                     <option value="UAH">UAH</option>
                     <option value="RUB">RUB</option>
             </select>
-            <label htmlFor="text">Ваше сообщение</label>
+            <label htmlFor="text">Ваше повідомлення</label>
             <textarea 
                 id="text"
                 name="text"
             />
             <label className="checkbox">
                 <input name="terms" type="checkbox" />
-                Соглашаетесь с политикой конфиденциальности?
+                Згодні з політикою конфіденційоності?
             </label>
-            <button type="submit">Отправить</button>
+            <button type="submit">Відправити</button>
         </form>
     )
 }
