4a56773c7a33f3adf90d519b9ac8213dadae83c7
Alexander0908 <sanya090895@gmail.com>
2024-07-25 23:38:59 +0300

    validation
    validation

--- 183-formik-mini-project/my-app/src/Form.js
+++ 183-formik-mini-project/my-app/src/Form.js
@@ -1,31 +1,78 @@
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
+const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = "Обов'язкове поле!";
+    } else if (values.name.length < 2) {
+        errors.name = "Мінімум два символи для заповнення"
+    }
+
+    if (!values.email) {
+        errors.email = "Обов'язкове поле!";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "Невірний емейл";
+    }
+
+    return errors;
+}
 
 const Form = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            amount: 0,
+            currency: '',
+            text: '',
+            terms: false
+        },
+        validate,
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    })
+
     return (
-        <form className="form">
-            <h2>Відправити пожертвування</h2>
+        <form className="form" onSubmit={formik.handleSubmit}>
+            <h2>Відправити Донат</h2>
             <label htmlFor="name">Ваше ім'я</label>
             <input
                 id="name"
                 name="name"
                 type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
+            {formik.errors.name && formik.touched.name ? <div>{formik.errors.name}</div> : null}
             <label htmlFor="email">Ваша пошта</label>
             <input
                 id="email"
                 name="email"
                 type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
+            {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
             <label htmlFor="amount">Кількість</label>
             <input
                 id="amount"
                 name="amount"
                 type="number"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
             <label htmlFor="currency">Валюта</label>
             <select
                 id="currency"
-                name="currency">
+                name="currency"
+                value={formik.values.currency}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}>
                     <option value="">Оберіть валюту</option>
                     <option value="USD">USD</option>
                     <option value="UAH">UAH</option>
@@ -35,9 +82,17 @@ const Form = () => {
             <textarea 
                 id="text"
                 name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
             <label className="checkbox">
-                <input name="terms" type="checkbox" />
+                <input 
+                    name="terms" 
+                    type="checkbox" 
+                    value={formik.values.terms}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}/>
                 Згодні з політикою конфіденційоності?
             </label>
             <button type="submit">Відправити</button>
