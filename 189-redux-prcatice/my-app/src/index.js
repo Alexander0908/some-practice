8cc2093bb3e1562b531944b79d90d087526552b4
Alexander0908 <sanya090895@gmail.com>
2024-08-11 21:48:38 +0300

    Practicing Redux in new mini project
    Practicing Redux in new mini project

---
+++ 189-redux-prcatice/my-app/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
+// const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'});
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'});
+});
+
+// let state = reducer(initialState, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <>
+    
+    </>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
